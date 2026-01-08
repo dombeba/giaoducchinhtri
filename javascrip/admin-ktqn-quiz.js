63ba1d4d3e031152a2ed5846bd59616955ea98ac
dombeba <kevildolan@gmail.com>
2026-01-08 22:37:43 +0700

    cap nhat code lan moi

--- javascrip/admin-ktqn-quiz.js
+++ javascrip/admin-ktqn-quiz.js
@@ -22,7 +22,7 @@ const QUIZ_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRll8YoIR4
 // ✅ 2) LINK APPS SCRIPT (ghi/xóa)
 // (CORS chặn đọc response, nhưng gửi no-cors vẫn đi)
 const QUIZ_API_URL =
-  "https://script.google.com/macros/s/AKfycbynj-8QUyqFOYFM5ZG-_HmEYrTjQlknZK57GNRbfULNYZj4ab3SJ5EkYwzgIQYUWsYGhw/exec";
+  "https://script.google.com/macros/s/AKfycbzPp69bMvr5BEYp4lzup4hAo4qVswFH_lYjYiAZ3pVVCfqNuM1a_Vq_A6erYpZ6V5yxug/exec";
 
 // Cache local (phòng khi CSV lỗi)
 const QUIZ_CACHE_KEY = "KTQN_QUIZZES_CACHE_V1";
