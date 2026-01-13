164298b7ee373e1b0bf89f4fc4539a3f6fc8634b
dombeba <kevildolan@gmail.com>
2026-01-13 19:30:52 +0700

    cap nhat code lan moi

--- javascrip/ktqn-lam-bai.js
+++ javascrip/ktqn-lam-bai.js
@@ -200,7 +200,7 @@ document.addEventListener("DOMContentLoaded", async ()=>{
         score, maxScore
       });
 
-      location.href="ktqn-ketqua-cua-toi.html";
+      location.href="ktqn-ketqua.html";
     }
 
     if($("submitBtn")) $("submitBtn").addEventListener("click",()=>doSubmit(false));
